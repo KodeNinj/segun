1f9a7f9b05d9930411d74f41ff63797ff0592f07
kodeninj <femifadiya.segun@gmail.com>
2023-01-07 23:36:44 +0100

    Feature Update: Updated the design array

--- src/Component/Contact.jsx
+++ src/Component/Contact.jsx
@@ -3,8 +3,14 @@ import Title from './Title'
 
 const Contact = () => {
   return (
-    <section className='mt-[80px]'>
+    <section className='my-[80px] min-h-[100vh] px-[10px] md:px-[0]'>
        <Title small="CONTACTS" large="How To Reach Me"/>
+       <div className="container">
+        <div className="left"></div>
+        {/* the address section */}
+        <div className="right"></div>
+        {/* the form section */}
+       </div>
     </section>
   )
 }
