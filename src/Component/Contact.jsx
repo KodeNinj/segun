e3a16e618c5ee294a5ce72aa7eb15c8ce0041ab4
kodeninj <femifadiya.segun@gmail.com>
2023-02-12 15:25:30 +0100

    FEATURE UPATE: Fix the contact list responsiveness

--- src/Component/Contact.jsx
+++ src/Component/Contact.jsx
@@ -10,35 +10,35 @@ const Contact = () => {
        <p className='text-md md:text-lg text-center capitalize text-slate-300 mt-[12px]'>All roads leads to me. Get in touch with me using any of the channels below. </p>
 
       {/* the social box */}
-      <div className='md:w-[60%] w-full md:mx-[20%] h-[100px] gap-[20px]  rounded-[60px] overflow-hidden mt-[50px] flex items-center justify-center'>
+      <div className='md:w-[80%] w-full md:mx-[10%] mx-0 md:h-[100px] h-[80px] gap-[20px]  md:rounded-[60px] rounded-[5px] overflow-hidden mt-[50px] flex items-center justify-center'>
 
       {/* behance link */}
       <a href="https://www.behance.net/segunfadiya?tracking_source=search_projects%7Cfrontend" target='_blank' rel="noreferrer" className='h-full w-full'>
-          <div className='h-full w-full bg-red-100 flex gap-[10px] flex-col justify-center items-center hover:bg-[var(--secondary)] ease duration-[1s] hover:text-[var(--bg)] hover:shadow-inner cursor-pointer'>
-            <FaBehance size={40}/>
-            <p className='font-bold'>Behance</p>
+          <div className='h-full w-full bg-[var(--fff)] text-slate-100 flex gap-[10px] flex-col justify-center items-center hover:bg-[var(--sss)] ease duration-[1s] rounded-[5px]  hover:shadow-inner cursor-pointer'>
+            <FaBehance size={25}/>
+            <p className=' md:text-xl text-sm'>Behance</p>
           </div>
       </a>
       {/* Dribbble link */}
-      <a href="https://dribbble.com/i_Osborne" target='_blank' rel="noreferrer" className='h-full w-full'>
-        <div className='h-full w-full bg-red-100 flex gap-[10px] flex-col justify-center items-center ease duration-[1s] hover:text-[var(--bg)] hover:shadow-inner hover:bg-[var(--secondary)] cursor-pointer'>
-          <FaDribbble size={40}/>
-          <p className='font-bold'>Dribbble</p>
+      <a href="https://dribbble.com/i_Osborne" target='_blank' rel="noreferrer" className='md:block hidden h-full w-full'>
+        <div className='h-full w-full bg-[var(--fff)] text-slate-100 flex rounded-[5px]  gap-[10px] flex-col justify-center items-center ease duration-[1s]  hover:shadow-inner hover:bg-[var(--sss)] cursor-pointer'>
+          <FaDribbble size={25}/>
+          <p className=' md:text-xl text-sm'>Dribbble</p>
         </div>
       </a>
       {/* linkedin link */}
       <a href="https://ng.linkedin.com/in/femi-fadiya-segun-pelumi" target='_blank' rel="noreferrer" className='h-full w-full'>
-        <div className='h-full w-full bg-red-100 flex gap-[10px] flex-col justify-center items-center ease duration-[1s] hover:text-[var(--bg)] hover:shadow-inner hover:bg-[var(--secondary)] cursor-pointer'>
-          <FaLinkedinIn size={40}/>
-          <p className='font-bold'>LinkedIn</p>
+        <div className='h-full w-full bg-[var(--fff)] text-slate-100 flex gap-[10px] flex-col rounded-[5px] justify-center items-center ease duration-[1s]  hover:shadow-inner hover:bg-[var(--sss)] cursor-pointer'>
+          <FaLinkedinIn size={25}/>
+          <p className=' md:text-xl text-sm'>LinkedIn</p>
         </div>
       </a>
 
       {/* Whatsapp link */}
       <a href="http://wa.me/2347038329044" target='_blank' rel="noreferrer" className='h-full w-full'>
-          <div className='h-full w-full bg-red-100 flex gap-[10px] flex-col justify-center items-center ease duration-[1s] hover:text-[var(--bg)] hover:shadow-inner hover:bg-[var(--secondary)]'>
-          <FaWhatsapp size={40}/>
-          <p className='font-bold'>Whatsapp</p>
+          <div className='h-full w-full bg-[var(--fff)] text-slate-100 flex gap-[10px] flex-col justify-center items-center hover:bg-[var(--sss)] ease duration-[1s] rounded-[5px]  hover:shadow-inner cursor-pointers'>
+          <FaWhatsapp size={25}/>
+          <p className='md:text-xl text-sm'>Whatsapp</p>
           </div>
       </a>
 
