2119bc39f2c66a8620e3cd67218151011790c5b3
kodeninj <femifadiya.segun@gmail.com>
2023-05-28 09:09:56 -0700

    Update: Testing commit on new system

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React from 'react'
 import Contact from './Component/Contact'
 // import DevProjects from './Component/DevProjects'
 import Skills from './Component/skills'
@@ -6,28 +6,23 @@ import './index.css'
 import About from './sections/About'
 import Hero from './sections/Hero'
 import Project from './sections/Project'
-import Aos from 'aos'
-import "aos/dist/aos.css";
+// import Aos from 'aos'
+// import "aos/dist/aos.css";
 import { ToastContainer } from 'react-toastify';
 import Footer from './Component/Footer'
 // import 'react-toastify/dist/ReactToastify.css';
 
 
 const App = () => {
-  useEffect(() => {
-    Aos.init({duration: 2000});
-    
-  }, [])
-  
 
   return (
     <div className='relative bg-[var(--bg)] min-h-[120vh]  overflow-hidden'>
       
       <Hero/>
       <About/>
-      <Skills data-aos='fade-up' />
-      <Project data-aos='slide-up'/>
-      <Contact data-aos='flip-right'/>
+      <Skills  />
+      <Project />
+      <Contact />
       <Footer/>
       <ToastContainer />
       {/* <DevProjects/> */}
