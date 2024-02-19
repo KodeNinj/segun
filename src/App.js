ac2fb093600cd952d5d057a33a427db53aa41d61
KodeNinja <79848529+KodeNinj@users.noreply.github.com>
2024-02-19 10:11:29 +0100

    Update App.js
    remove AOS again

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ import './index.css'
 import About from './sections/About'
 import Hero from './sections/Hero'
 import Project from './sections/Project'
-import Aos from 'aos'
-import "aos/dist/aos.css";
+// import Aos from 'aos'
+// import "aos/dist/aos.css";
 import { ToastContainer } from 'react-toastify';
 import Footer from './sections/Footer'
 import 'react-toastify/dist/ReactToastify.css';
 
 
 const App = () => {
-  useEffect(() => {
-    Aos.init();
-    Aos.refresh();
-  }, [])
+  // useEffect(() => {
+  //   Aos.init();
+  //   Aos.refresh();
+  // }, [])
   return (
     <div className='relative bg-[var(--bg)] min-h-[120vh]  overflow-hidden'>
       
