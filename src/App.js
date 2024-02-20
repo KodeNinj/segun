9748746081cb4b60f1db1b81ae73badae68f9057
kodeninj <femifadiya.segun@gmail.com>
2024-02-20 08:30:31 +0100

    added cv files to the download cv button

--- src/App.js
+++ src/App.js
@@ -1,34 +1,33 @@
-import Contact from './sections/Contact'
-import { React,useEffect } from 'react'
-import Skills from './sections/skills'
-import './index.css'
-import About from './sections/About'
-import Hero from './sections/Hero'
-import Project from './sections/Project'
+import Contact from "./sections/Contact";
+import { React } from "react";
+// import { React,useEffect } from 'react'
+import Skills from "./sections/skills";
+import "./index.css";
+import About from "./sections/About";
+import Hero from "./sections/Hero";
+import Project from "./sections/Project";
 // import Aos from 'aos'
 // import "aos/dist/aos.css";
-import { ToastContainer } from 'react-toastify';
-import Footer from './sections/Footer'
-import 'react-toastify/dist/ReactToastify.css';
-
+import { ToastContainer } from "react-toastify";
+import Footer from "./sections/Footer";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-  // useEffect(() => {
-  //   Aos.init();
-  //   Aos.refresh();
-  // }, [])
-  return (
-    <div className='relative bg-[var(--bg)] min-h-[120vh]  overflow-hidden'>
-      
-      <Hero/>
-      <About/>
-      <Skills  />
-      <Project />
-      <Contact />
-      <Footer/>
-      <ToastContainer />
-    </div>
-  )
-}
+	// useEffect(() => {
+	//   Aos.init();
+	//   Aos.refresh();
+	// }, [])
+	return (
+		<div className="relative bg-[var(--bg)] min-h-[120vh]  overflow-hidden">
+			<Hero />
+			<About />
+			<Skills />
+			<Project />
+			<Contact />
+			<Footer />
+			<ToastContainer />
+		</div>
+	);
+};
 
-export default App
+export default App;
